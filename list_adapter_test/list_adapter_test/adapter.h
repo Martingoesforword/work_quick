4369699345a10d72dc6f1fe33920bf45e59f4f99
songjiaheng <song8713@qq.com>
2019-10-14 19:51:38 +0800

    滚动条适配器

---
+++ list_adapter_test/list_adapter_test/adapter.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "ui.h"
+
+using namespace std;
+  
+template<typename control_struct, typename data_struct> 
+class ListUpdate
+{
+private:
+	vector< control_struct> cons;
+	vector< data_struct> datas;
+	int control_index;
+	void(*func)(control_struct, data_struct, int index);
+	I_ScrollBar_Adv* scr;
+public:
+	ListUpdate(I_ScrollBar_Adv* scrs, vector< control_struct>& conss, vector<data_struct>& datass, void(*funcc)(control_struct, data_struct, index int))
+		:cons(conss), datas(datass), func(funcc), control_index(0), scr(scrs)
+	{
+		   
+	}
+	void update()
+	{
+		 
+		//滚动条m_scr_content_friend刷新
+		int maxPageNum = (datas.size() + cons.size() - 1) / cons.size();
+		if (maxPageNum == 1 || maxPageNum == 0) scr->ShowWindow(false);
+		else scr->ShowWindow(true);
+
+		//列表m_s_friend_control_arr刷新
+		vector<data_struct>::iterator  pos = datas.begin();
+		std::advance(pos, control_index);
+
+		for (int i = 0; pos != datas.end() && i < control_index; ++pos)
+		{
+			func(cons[i], datas[control_index+i]);
+			i++;
+		} 
+	}
+
+};
+
+
