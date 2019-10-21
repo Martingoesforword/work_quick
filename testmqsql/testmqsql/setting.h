6bba90bb5520b1229e7302b50019e7ff9633f324
songjiaheng <song8713@qq.com>
2019-10-21 16:21:31 +0800

    布局解析提交 项目over

---
+++ testmqsql/testmqsql/setting.h
@@ -0,0 +1,9 @@
+#pragma once
+#define PATH_MUSIC_T "F:\\workspace\\a\\trunc\\exe\\NewUIEditor\\resources\\media\\ui\\music_t"
+#define SERVER_ADDR "139.155.25.107"
+#define SERVER_NAME "root"
+#define SERVER_PASSWARD "mysqlsong8713"
+#define SERVER_DBNAME "quick_work"
+#define SERVER_PROT 3306
+
+#define ITEM_NUM_PER_SEND 1000
