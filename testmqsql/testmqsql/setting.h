3bcfd2880876fad00b03af26a5d55e882f6b13a3
songjiaheng <song8713@qq.com>
2019-11-08 15:41:33 +0800

    1

--- testmqsql/testmqsql/setting.h
+++ testmqsql/testmqsql/setting.h
@@ -1,9 +1,13 @@
 #pragma once
 #define PATH_MUSIC_T "F:\\workspace\\a\\trunc\\exe\\NewUIEditor\\resources\\media\\ui\\music_t"
-#define SERVER_ADDR "139.155.25.107"
-#define SERVER_NAME "root"
-#define SERVER_PASSWARD "mysqlsong8713"
+//#define SERVER_ADDR "139.155.25.107"
+#define SERVER_ADDR "172.17.100.47"
+//#define SERVER_NAME "root"
+#define SERVER_NAME "hardcore"
+//#define SERVER_PASSWARD "mysqlsong8713"
+#define SERVER_PASSWARD "111111"
 #define SERVER_DBNAME "quick_work"
 #define SERVER_PROT 3306
 
 #define ITEM_NUM_PER_SEND 1000
+#define FIRST_FUN
