134eb7c25564224e56153cc8180c8c9f68189105
songjiaheng <song8713@qq.com>
2019-12-19 11:43:07 +0800

    增加caption和zlevel

--- csv_for_search/testmqsql/setting.h
+++ csv_for_search/testmqsql/setting.h
@@ -1,9 +1,18 @@
 #pragma once
 #define PATH_STRINGS_ZH_CN "F:\\workspace\\a\\trunc\\exe\\resources\\strings\\zh_cn"
+#define SERVER2
+#ifdef SERVER1
+#define SERVER_ADDR "172.17.100.114"
+#define SERVER_NAME "hardcore"
+#define SERVER_PASSWARD "111111"
+
+#endif
+#ifdef SERVER2
 #define SERVER_ADDR "139.155.25.107"
 #define SERVER_NAME "root"
 #define SERVER_PASSWARD "mysqlsong8713"
-#define SERVER_DBNAME "quick_work"
+#endif
+#define SERVER_DBNAME "csv"
 #define SERVER_PROT 3306
 
 #define ITEM_NUM_PER_SEND 1000
--- testmqsql/testmqsql/setting.h
+++ testmqsql/testmqsql/setting.h
@@ -1,11 +1,17 @@
 #pragma once
 #define PATH_MUSIC_T "F:\\workspace\\a\\trunc\\exe\\NewUIEditor\\resources\\media\\ui\\music_t"
-//#define SERVER_ADDR "139.155.25.107"
-#define SERVER_ADDR "172.17.100.47"
-//#define SERVER_NAME "root"
+#define SERVER1
+#ifdef SERVER1
+#define SERVER_ADDR "172.17.100.114"
 #define SERVER_NAME "hardcore"
-//#define SERVER_PASSWARD "mysqlsong8713"
 #define SERVER_PASSWARD "111111"
+
+#endif
+#ifdef SERVER2
+#define SERVER_ADDR "139.155.25.107"
+#define SERVER_NAME "root"
+#define SERVER_PASSWARD "mysqlsong8713"
+#endif
 #define SERVER_DBNAME "quick_work"
 #define SERVER_PROT 3306
 
